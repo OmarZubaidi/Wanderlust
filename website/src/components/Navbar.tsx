aab8875edc2e36d2f98a1d68f7af54d060f87374
daniele24134 <capanodaniele@outlook.it>
2022-05-07 23:18:38 +0200

    feat: avatar dropdown

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/Navbar.module.css';
 import { logged } from '../../mockAuth';
+import Link from 'next/link';
 
 export const Navbar: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const show = open ? 'show' : undefined;
+
   return (
     <div className={styles.navbar_container}>
       <nav className={styles.navbar}>
         <h1 className={styles.logo}>Wanderlust</h1>
         {logged ? (
           <div className='buttons'>
-            <button className='avatar'></button>
-            <button className={'signup_button button'}>Logout</button>
+            <div className={`avatar_dropdown ${show}`}>
+              <Link href={'/'}>
+                <a>Account</a>
+              </Link>
+              <Link href={'/'}>
+                <a id='logout_button'>Logout</a>
+              </Link>
+            </div>
+            <button
+              onClick={() => setOpen((prev) => !prev)}
+              className='avatar'
+            ></button>
           </div>
         ) : (
           <div className='buttons'>
