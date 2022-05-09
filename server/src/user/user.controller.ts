a3cf741bbc1ff8d4b2a8cc691e7cb47b536af896
stefanfeldner <stefanfeldner@gmail.com>
2022-05-09 15:49:48 +0200

    feat: Full CRUD operations for users

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from './dtos/user';
 import { UserService } from './user.service';
 
 @Controller('users')
@@ -8,5 +17,30 @@ export class UserController {
   @Get()
   findAll() {
     return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.userService.findById(id);
+  }
+
+  @Get('/email/:email')
+  findByEmail(@Param('email') email: string) {
+    return this.userService.findByEmail(email);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Delete(':id')
+  deleteById(@Param('id') id: string) {
+    return this.userService.deleteById(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
   }
 }
